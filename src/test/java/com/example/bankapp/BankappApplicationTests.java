338961c56c5b6cb1d7440c54f4e18190ad1d19f5
mukul165 <61845531+mukul165@users.noreply.github.com>
2026-07-29 08:31:14 +0530

    Import ActiveProfiles and update test class
    Added ActiveProfiles annotation to prevent MySQL errors during tests.

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.bankapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // <-- MUST IMPORT THIS
 
 @SpringBootTest
+@ActiveProfiles("test") // <-- THIS LINE IS REQUIRED TO STOP MYSQL ERRORS
 class BankappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
